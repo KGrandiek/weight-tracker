bca2b7dad4e1e7eef1166238549d589aa2f4132a
kellygrandiek <kelly.grandiek@gmail.com>
2025-06-30 16:21:24 +1200

    wip

--- src/app/hero/hero.ts
+++ src/app/hero/hero.ts
@@ -8,5 +8,29 @@ import { Component, Input } from '@angular/core';
   styleUrl: './hero.scss'
 })
 export class Hero {
-  @Input() data = [];
+  @Input({ required: true }) data!: any[];
+
+  weightLoss = 0;
+  waistLoss = 0;
+  stomachLoss = 0;
+  thighLoss = 0;
+
+  ngOnInit(): void {
+    console.log('Value of data:', this.data);
+
+    this.weightLoss = this.getLoss('Weight');
+    this.waistLoss = this.getLoss('Waist');
+    this.stomachLoss = this.getLoss('Stomach');
+    this.thighLoss = this.getLoss('Thigh');
+  }
+
+  getLoss(name: string): number {
+    const entry = this.data.find(d => d.name === name);
+    if (entry && entry.data.length > 1) {
+      const first = entry.data[0].measurement;
+      const last = entry.data[entry.data.length - 1].measurement;
+      return +(first - last).toFixed(1);
+    }
+    return 0;
+  }
 }
